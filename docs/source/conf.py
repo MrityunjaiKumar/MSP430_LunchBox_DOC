c1c3b9adad6dc25e9186be3a740aadb0ee775378
MrityunjaiKumar <mrityunjai.kmr@gmail.com>
2020-02-11 23:54:41 +0530

    favicon updated

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -83,7 +83,7 @@ html_theme_options = {
     'collapse_navigation' : True,
     'logo_only': True,
 }
-
+html_favicon = 'favicon.ico'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
